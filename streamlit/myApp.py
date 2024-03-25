0ee18e742d00b313e5db19f599861ed2316011ce
junta <junta8918@gmail.com>
2024-03-25 09:37:49 +0900

    add reports

---
+++ streamlit/myApp.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Arbitrum Governance Data Analytics")
+# st.sidebar.markdown("# Gitcoin Open Data Science Hackathon")
+
+st.markdown(
+    """
+
+"""
+)
